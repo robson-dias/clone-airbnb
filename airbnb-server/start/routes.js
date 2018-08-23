863d3a045b3a3788c35f95a68dfd61848083ff37
Robson Dias <robsondiaz@gmail.com>
2018-08-23 17:19:14 -0300

    Upload de imagens e geolocalização no AdonisJS

--- airbnb-server/start/routes.js
+++ airbnb-server/start/routes.js
@@ -27,3 +27,7 @@ Route.post("/sessions", "SessionController.create");
 Route.resource("properties", "PropertyController")
   .apiOnly()
   .middleware("auth");
+
+Route.post("properties/:id/images", "ImageController.store").middleware("auth");
+
+Route.get("images/:path", "ImageController.show");
